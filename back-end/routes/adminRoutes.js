6ea9bace5aadfdcd862813a336581cbf57803e7b
jfmrestore@gmail.com <jfmrestore@gmail.com>
2021-04-30 12:52:14 +0200

    Add updates virginie signin

--- back-end/routes/adminRoutes.js
+++ back-end/routes/adminRoutes.js
@@ -7,7 +7,7 @@ const router = require("express").Router();
 const adminController = require("../controllers/controllersAdmin");
 
 router.post("/dbInitModules", adminController.dbInitModules);
-
 router.post("/dbAddAdminUsers", adminController.dbAddAdminUsers);
+router.post("/signIn", adminController.signIn);
 
 module.exports = router;
