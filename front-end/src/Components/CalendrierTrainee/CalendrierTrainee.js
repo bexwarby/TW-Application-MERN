c15bb0d237aacafcbca1577278d3f154b243fec6
Goubault Virginie <virginie.goubault@gmail.com>
2021-05-04 10:41:07 +0200

    calendrier trainee avec fetch

--- front-end/src/Components/CalendrierTrainee/CalendrierTrainee.js
+++ front-end/src/Components/CalendrierTrainee/CalendrierTrainee.js
@@ -3,19 +3,19 @@ import React, { useState } from "react";
 import Calendar from "react-calendar";
 
 function CalendrierTrainee() {
-    /*function envoyer date*/
-  const [rdv, setRdv] = useState(new Date());
-  const onChange = (rdv) => {
-    setRdv(rdv);
-    console.log(rdv);
+  /*function envoyer date*/
+  const [day, setDay] = useState(new Date());
+  const onChange = (day) => {
+    setDay(day);
+    //console.log(day);
   };
-/*dates NON disponibles*/
+  /*dates NON disponibles*/
   const disabledDates = [
-  new Date(2021, 4, 10),
-  new Date(2021, 4, 13),
-  new Date(2021, 4, 25),
-];
-/*function envoyer horaire*/
+    new Date(2021, 4, 10),
+    new Date(2021, 4, 13),
+    new Date(2021, 4, 25),
+  ];
+  /*function envoyer horaire*/
   const [horaire, setHoraire] = useState({});
   function handleChange(e) {
     let sendHoraire = {
@@ -23,42 +23,87 @@ function CalendrierTrainee() {
       [e.target.name]: e.target.value,
     };
     setHoraire(sendHoraire);
+    //console.log(sendHoraire)
   }
-/*function envoyer date ET horaire*/
-  async function submitRdv(event) {
-    const option ={
-        method : "POST",
-        headers : {
-            "Content-Type" : "application/json",
-            Authorization : "bearer",
+  /*function envoyer date ET horaire*/
+  /*async function submitRdv(event) {
+    const message = await fetch(
+      `${process.env.REACT_APP_SERVER}/trainee/calendrier`,
+      {
+        method: "POST",
+        headers: {
+          Accept: "application/json",
+          "Content-Type": "application/json",
         },
         body: JSON.stringify({
-            date : rdv,
-            time : horaire
+          date: day,
+          horaire: horaire,
         }),
-    };
-  }
+      }
+    );
+    console.log(message);
+    const data = await message.json();
+    console.log(data);
+  }*/
+
+  const submitRdv = async () => {
+    /*fetch*/
+
+    try {
+      const response = await fetch(
+        `${process.env.REACT_APP_SERVER}/trainee/calendrier`,
+        {
+          method: "POST",
+          headers: {
+            Accept: "application/json",
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            date: day,
+            horaire: horaire,
+          }),
+        }
+      );
+      const res = await response.json();
+      console.log(res);
+    } catch (err) {
+      console.log(err);
+    }
+  };
 
   return (
     <div>
-      <Calendar onChange={onChange} value={rdv} tileDisabled={({date, view}) =>
-                    (view === 'month') && // Block day tiles only
-                    disabledDates.some(disabledDate =>
-                      date.getFullYear() === disabledDate.getFullYear() &&
-                      date.getMonth() === disabledDate.getMonth() &&
-                      date.getDate() === disabledDate.getDate()
-                    )} />
+      <Calendar
+        onChange={onChange}
+        name="day"
+        value={day}
+        tileDisabled={({ date, view }) =>
+          view === "month" && // Block day tiles only
+          disabledDates.some(
+            (disabledDate) =>
+              date.getFullYear() === disabledDate.getFullYear() &&
+              date.getMonth() === disabledDate.getMonth() &&
+              date.getDate() === disabledDate.getDate()
+          )
+        }
+      />
 
       <p>Choose time</p>
-      <select id="horaire" name="horaire" size="" onChange={handleChange}>
-        <option value="1">8-9</option>
-        <option value="2">9-10</option>
-        <option value="3">10-11</option>
-        <option value="4">11-12</option>
-        <option value="5">13-14</option>
-        <option value="6">14-15</option>
-        <option value="7">15-16</option>
-        <option value="8">16-17</option>
+      <select
+        id="horaire"
+        name="horaire"
+        size=""
+        onChange={handleChange}
+        value="horaireSend"
+      >
+        <option value="8-9">8-9</option>
+        <option value="9-10">9-10</option>
+        <option value="10-11">10-11</option>
+        <option value="11-12">11-12</option>
+        <option value="13-14">13-14</option>
+        <option value="14-15">14-15</option>
+        <option value="15-16">15-16</option>
+        <option value="16-17">16-17</option>
       </select>
 
       <button type="button" onClick={submitRdv}>
