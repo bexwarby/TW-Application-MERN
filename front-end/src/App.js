4e5a125988115240ab1a5f3f4b3bb260460d8959
Rebekah Munoz Warburton <bex.warburton@gmail.com>
2021-05-06 15:33:39 +0200

    Routes à jour FE

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -8,7 +8,7 @@ import "./App.css";
 import Home from "./Components/PageHome/PageHome";
 import TraineeSignIn from "./Components/TraineeSignIn/TraineeSignIn";
 import TraineeSignUp from "./Components/TraineeSignUp/TraineeSignUp";
-import InstructorSignIn from "./Components/InstructorSignIn/InstructorSignIn";
+import InstructorSignIn from "./Components/instructorSignIn/instructorSignIn";
 //////Trainee profile///
 import TraineeChoiceModule from "./Components/TraineeProfile/1Module/TraineeChoiceModule";
 import TraineeEmail from "./Components/TraineeProfile/2Email/TraineeEmail";
@@ -20,8 +20,8 @@ import TraineeTimeForTrained from "./Components/TraineeProfile/7TimeForTrained/T
 import TraineeLanguage from "./Components/TraineeProfile/8Language/TraineeLanguage";
 import TraineeBirth from "./Components/TraineeProfile/9Birth/TraineeBirth";
 import TraineeComment from "./Components/TraineeProfile/10AddComment/TraineeComment";
-//import InstructorSignUp from "./navigation/InstructorSignUp";
-//import SignUpPilot from "./Components/InstructorSignUp/SignUp";
+//import InstructorSignUp from "./navigation/instructorSignUp";
+//import SignUpPilot from "./Components/instructorSignUp/SignUp";
 //import InstructorHome from "./Components/PageInstructor/PageInstructor";
 //import TraineeHome from "./Components/PageTrainee/PageTrainee";
 //import AdminHome from "./Components/PageAdmin/PageAdmin";
@@ -31,23 +31,23 @@ import { StateMachineProvider } from "little-state-machine";
 import PageAdmin from "./Components/PageAdmin/PageAdmin";
 import CalendrierTrainee from "./Components/CalendrierTrainee/CalendrierTrainee";
 
-import Welcome from "./Components/InstructorSignUp/Welcome/Welcome";
-import Name from "./Components/InstructorSignUp/1Name/Name";
-import HoursTotal from "./Components/InstructorSignUp/2HoursTotal/HoursTotal";
-import Ratings from "./Components/InstructorSignUp/3Ratings/Ratings";
-import Types from "./Components/InstructorSignUp/4Types/Types";
-import Equipment from "./Components/InstructorSignUp/5Equipment/Equipment";
-import Software from "./Components/InstructorSignUp/6Software/Software";
-import HoursClass from "./Components/InstructorSignUp/7HoursClass/HoursClass";
-import Time from "./Components/InstructorSignUp/8Time/Time";
-import Language from "./Components/InstructorSignUp/9Language/Language";
-import LicenseFile from "./Components/InstructorSignUp/10LicenseFile/LicenseFile";
-import Birthday from "./Components/InstructorSignUp/11Birthday/Birthday";
-import Bio from "./Components/InstructorSignUp/12Bio/Bio";
-import Email from "./Components/InstructorSignUp/13Email/Email";
-import Password from "./Components/InstructorSignUp/14bisPassword/Password";
-import Submit from "./Components/InstructorSignUp/14Submit/Submit";
-import Result from "./Components/InstructorSignUp/Result/Result";
+import Welcome from "./Components/instructorSignUp/Welcome/Welcome";
+import Name from "./Components/instructorSignUp/1Name/Name";
+import HoursTotal from "./Components/instructorSignUp/2HoursTotal/HoursTotal";
+import Ratings from "./Components/instructorSignUp/3Ratings/Ratings";
+import Types from "./Components/instructorSignUp/4Types/Types";
+import Equipment from "./Components/instructorSignUp/5Equipment/Equipment";
+import Software from "./Components/instructorSignUp/6Software/Software";
+import HoursClass from "./Components/instructorSignUp/7HoursClass/HoursClass";
+import Time from "./Components/instructorSignUp/8Time/Time";
+import Language from "./Components/instructorSignUp/9Language/Language";
+import LicenseFile from "./Components/instructorSignUp/10LicenseFile/LicenseFile";
+import Birthday from "./Components/instructorSignUp/11Birthday/Birthday";
+import Bio from "./Components/instructorSignUp/12Bio/Bio";
+import Email from "./Components/instructorSignUp/13Email/Email";
+import Password from "./Components/instructorSignUp/14bisPassword/Password";
+import Submit from "./Components/instructorSignUp/14Submit/Submit";
+import Result from "./Components/instructorSignUp/Result/Result";
 import React from "react";
 
 function App() {
@@ -75,60 +75,62 @@ function App() {
             <Route path="/usr/instructor/signin">
               <InstructorSignIn />
             </Route>
+            {/* TraineeProfile */}
             <React.Fragment>
               <Route
-                path="/Trainee/profile/step1"
+                path="/trainee/profile/step1"
                 component={TraineeChoiceModule}
               />
-              <Route path="/Trainee/profile/step2" component={TraineeEmail} />
+              <Route path="/trainee/profile/step2" component={TraineeEmail} />
               <Route
-                path="/Trainee/profile/step3"
+                path="/trainee/profile/step3"
                 component={TraineeFlyHours}
               />
               <Route
-                path="/Trainee/profile/step4"
+                path="/trainee/profile/step4"
                 component={TraineeSimEquipment}
               />
               <Route
-                path="/Trainee/profile/step5"
+                path="/trainee/profile/step5"
                 component={TraineeSoftware}
               />
               <Route
-                path="/Trainee/profile/step6"
+                path="/trainee/profile/step6"
                 component={TraineeWeekStart}
               />
               <Route
-                path="/Trainee/profile/step7"
+                path="/trainee/profile/step7"
                 component={TraineeTimeForTrained}
               />
               <Route
-                path="/Trainee/profile/step8"
+                path="/trainee/profile/step8"
                 component={TraineeLanguage}
               />
-              <Route path="/Trainee/profile/step9" component={TraineeBirth} />
+              <Route path="/trainee/profile/step9" component={TraineeBirth} />
               <Route
-                path="/Trainee/profile/step10"
+                path="/trainee/profile/step10"
                 component={TraineeComment}
               />
             </React.Fragment>
+            {/* IntructorSignUp */}
             <React.Fragment>
-              <Route path="/Welcome" component={Welcome} />
-              <Route path="/Instructor/signup/step1" component={Name} />
-              <Route path="/Instructor/signup/step2" component={HoursTotal} />
-              <Route path="/Instructor/signup/step3" component={Ratings} />
-              <Route path="/Instructor/signup/step4" component={Types} />
-              <Route path="/Instructor/signup/step5" component={Equipment} />
-              <Route path="/Instructor/signup/step6" component={Software} />
-              <Route path="/Instructor/signup/step7" component={HoursClass} />
-              <Route path="/Instructor/signup/step8" component={Time} />
-              <Route path="/Instructor/signup/step9" component={Language} />
-              <Route path="/Instructor/signup/step10" component={LicenseFile} />
-              <Route path="/Instructor/signup/step11" component={Birthday} />
-              <Route path="/Instructor/signup/step12" component={Bio} />
-              <Route path="/Instructor/signup/step13" component={Email} />
-              <Route path="/Instructor/signup/step14" component={Password} />
-              <Route path="/Instructor/signup/submit" component={Submit} />
-              <Route path="/Instructor/signup/result" component={Result} />
+              <Route path="/welcome" component={Welcome} />
+              <Route path="/instructor/signup/step1" component={Name} />
+              <Route path="/instructor/signup/step2" component={HoursTotal} />
+              <Route path="/instructor/signup/step3" component={Ratings} />
+              <Route path="/instructor/signup/step4" component={Types} />
+              <Route path="/instructor/signup/step5" component={Equipment} />
+              <Route path="/instructor/signup/step6" component={Software} />
+              <Route path="/instructor/signup/step7" component={HoursClass} />
+              <Route path="/instructor/signup/step8" component={Time} />
+              <Route path="/instructor/signup/step9" component={Language} />
+              <Route path="/instructor/signup/step10" component={LicenseFile} />
+              <Route path="/instructor/signup/step11" component={Birthday} />
+              <Route path="/instructor/signup/step12" component={Bio} />
+              <Route path="/instructor/signup/step13" component={Email} />
+              <Route path="/instructor/signup/step14" component={Password} />
+              <Route path="/instructor/signup/submit" component={Submit} />
+              <Route path="/instructor/signup/result" component={Result} />
             </React.Fragment>
             {/*<InstructorHome />*/}
             {/* <TraineeHome /> */}
