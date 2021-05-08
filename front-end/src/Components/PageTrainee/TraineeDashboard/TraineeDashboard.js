d28c30ad5c47ff0148c1a8e29e023b68bb09c9ed
Rebekah Munoz Warburton <bex.warburton@gmail.com>
2021-05-08 10:08:16 +0200

    ajout de react-circular-progressbar

--- front-end/src/Components/PageTrainee/TraineeDashboard/TraineeDashboard.js
+++ front-end/src/Components/PageTrainee/TraineeDashboard/TraineeDashboard.js
@@ -2,15 +2,51 @@
  * Dashboard de l'eleve
  */
 
+import { CircularProgressbar } from 'react-circular-progressbar';
+import 'react-circular-progressbar/dist/styles.css';
 import './TraineeDashboard.css'
 
 
 function TraineeDashboard() {
- 
+
+  const value = 0;
+  const maxValue = 25;
+  const level = level;
+
+  function onLoad () {
+    if (level == "private") {
+      return {
+        minValue: 0,
+        maxValue: 15,
+        value: 10
+      } // faut ajouter les heures accumulés
+    }
+    else if (level == "professional") {
+      return {
+        minValue: 0,
+        maxValue: 25,
+        value: 10
+      } // faut ajouter les heures accumulés
+    }
+    else if (level == "expert") {
+      return {
+        minValue: 0,
+        maxValue: 20,
+        value: 10
+      } // faut ajouter les heures accumulés
+    }
+  }
 
   return (
     <div>
-      <h3>Dashboard eleve</h3>
+      <h3>Dashboard</h3>
+      <div style={{ width: 200, height: 200 }}>
+        <CircularProgressbar 
+          onLoad={onLoad}
+          value={value} 
+          maxValue={maxValue} 
+          text={`${value / maxValue * 100}%`}/>
+      </div>
     </div>
   );
 }
