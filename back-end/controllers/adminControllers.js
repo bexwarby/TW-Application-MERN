74b4a7b9f0a8ba66fe42e6d4125c5b352fa6a817
link <8costa21@gmail.com>
2021-05-09 04:27:35 +0200

    delete admin

--- back-end/controllers/adminControllers.js
+++ back-end/controllers/adminControllers.js
@@ -8,7 +8,6 @@ const jwt = require('jsonwebtoken')
 const adminController = {
   signIn: async (req, res) => {
     const adminUser = await User.findOne({ email: req.body.email, role: "admin" });
-    console.log(adminUser);
 
     try {
 
@@ -54,21 +53,33 @@ const adminController = {
       role: "admin",
     });
 
+    console.log(docAdmin);
     docAdmin.save((err) => {
       if (err) {
         res.status(501).json({ message: err.message });
       } else {
-        res.json({ message: "New admin created!" });
+        res.status(201).json({ message: "New admin created!" });
       }
     });
+  },
+
+  delete: async (req, res) => {
+    const userId = req.params.id
+    try {
+      await User.findOneAndDelete({ _id: userId }, (err, userDoc) => {
+        return (err) ? res.status(500).json({ msg: err }) : res.status(200).json({ msg: 'user deleted' })
+      })
+    } catch (err) {
+      return res.status(500).json({ msg: err })
+    }
   },
 
   pending: async (req, res) => {
     try {
       const pendingUser = await User.find({ role: "pending-instructor" });
-      console.log(pendingUser)
-      if (!pendingUser) res.status(400).json({ message: "Aucun utilisateur à valider" })
-      else res.status(200).json({ pendings: pendingUser })
+      if (!pendingUser) return res.status(400).json({ message: "Aucun utilisateur à valider" })
+      else return res.status(200).json({ pendings: pendingUser })
+
     } catch (err) {
       console.log(err);
     }
@@ -81,13 +92,22 @@ const adminController = {
         userId, { role: 'instructor' }, { new: true }, (err, doc) => {
           if (err) { return res.status(400).json({ message: err }) }
           else { return res.status(201).json({ message: 'Instructor validated', instructor: doc }) }
-        }
-      )
+        })
 
     } catch (err) {
       console.log(err);
     }
-    // console.log(adminUser);
+  },
+
+  getUsers: async (req, res) => {
+    try {
+      const users = await User.find({}).sort('role fullName')
+      if (!users) return res.status(400).json({ message: "Aucun utilisateur" })
+      else return res.status(200).json({ users })
+
+    } catch (err) {
+      console.log(err);
+    }
   }
 };
 
